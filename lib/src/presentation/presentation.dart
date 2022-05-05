e78ba5ec84217e0d86d269b2aa8b912e3c14e14c
jtdLab <72231111+jtdLab@users.noreply.github.com>
2022-05-05 22:41:38 +0200

    remove platform specific dependencies

--- lib/src/presentation/presentation.dart
+++ lib/src/presentation/presentation.dart
@@ -4,12 +4,7 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 export 'package:auto_route/auto_route.dart';
-export 'package:cupertino_icons/cupertino_icons.dart';
 export 'package:flutter_bloc/flutter_bloc.dart';
-// TODO
-//export 'package:macos_ui/macos_ui.dart';
-//export 'package:fluent_ui/fluent_ui.dart';
-//export 'package:yaru/yaru.dart';
 
 part 'flow.dart';
 part 'mixins.dart';
