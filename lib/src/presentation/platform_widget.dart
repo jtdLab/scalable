f7a365612b7381213af3867cc65fa1314dbddcee
jtdLab <72231111+jtdLab@users.noreply.github.com>
2022-05-10 00:44:18 +0200

    add platform widget

---
+++ lib/src/presentation/platform_widget.dart
@@ -0,0 +1,69 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+
+class PlatformNotSupported extends Error {
+  @override
+  String toString() => 'Platform not supported.';
+}
+
+/// Buildes the widget provided depending on platform the application runs on.
+class PlatformWidget extends StatelessWidget {
+  final Widget Function(BuildContext)? android;
+  final Widget Function(BuildContext)? ios;
+  final Widget Function(BuildContext)? web;
+  final Widget Function(BuildContext)? macos;
+  final Widget Function(BuildContext)? linux;
+  final Widget Function(BuildContext)? windows;
+
+  const PlatformWidget({
+    Key? key,
+    this.android,
+    this.ios,
+    this.web,
+    this.macos,
+    this.linux,
+    this.windows,
+  })  : assert(android != null ||
+            ios != null ||
+            web != null ||
+            macos != null ||
+            linux != null ||
+            windows != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    if (kIsWeb) {
+      final widget = web?.call(context);
+
+      if (widget != null) {
+        return widget;
+      }
+    } else {
+      late final Widget? widget;
+      if (Platform.isAndroid) {
+        widget = android?.call(context);
+      }
+      if (Platform.isIOS) {
+        widget = ios?.call(context);
+      }
+      if (Platform.isMacOS) {
+        widget = macos?.call(context);
+      }
+      if (Platform.isLinux) {
+        widget = linux?.call(context);
+      }
+      if (Platform.isWindows) {
+        widget = windows?.call(context);
+      }
+
+      if (widget != null) {
+        return widget;
+      }
+    }
+
+    throw PlatformNotSupported();
+  }
+}
