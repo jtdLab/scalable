8c6eff35a9cf4b8799c0ad0f940c6bc379a91d2b
jtdLab <72231111+jtdLab@users.noreply.github.com>
2022-05-19 23:34:45 +0200

    move container to this package

--- lib/src/core/core.dart
+++ lib/src/core/core.dart
@@ -1,4 +1,10 @@
+import 'package:get_it/get_it.dart';
+
 export 'package:dartz/dartz.dart' hide emptyMap, emptySet;
-export 'package:freezed_annotation/freezed_annotation.dart' hide reverse;
+export 'package:freezed_annotation/freezed_annotation.dart';
 export 'package:injectable/injectable.dart';
 export 'package:kt_dart/kt.dart';
+export 'package:get_it/get_it.dart';
+
+// TODO doc
+final getIt = GetIt.instance;
