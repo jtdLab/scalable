730f539f56a2979cbe362aa15d5f05b6a0d6479b
jtdLab <72231111+jtdLab@users.noreply.github.com>
2022-05-20 00:02:51 +0200

    organize

--- lib/src/core/core.dart
+++ lib/src/core/core.dart
@@ -2,9 +2,9 @@ import 'package:get_it/get_it.dart';
 
 export 'package:dartz/dartz.dart' hide emptyMap, emptySet;
 export 'package:freezed_annotation/freezed_annotation.dart';
+export 'package:get_it/get_it.dart';
 export 'package:injectable/injectable.dart';
 export 'package:kt_dart/kt.dart';
-export 'package:get_it/get_it.dart';
 
 // TODO doc
 final getIt = GetIt.instance;
